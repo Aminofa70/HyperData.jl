2fc0ec51e5752f7144f3ca137cb60801d3cedabc
Documenter.jl <documenter@juliadocs.github.io>
2024-07-20 23:24:22 +0000

    build based on 83a93ac

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HyperData","category":"page"},{"location":"#HyperData","page":"Home","title":"HyperData","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HyperData.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HyperData]","category":"page"}]
+}
