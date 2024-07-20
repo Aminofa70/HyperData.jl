b93ba5efdb9858b45b98293cb1c100da591afe22
Aminofa70 <amin.alibakhshi@upm.es>
2024-07-21 01:59:13 +0200

    change in utils again

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,4 @@
-import  DelimitedFiles
+using DelimitedFiles
 
 """
     function to read  experimental data for some hyperelastic materials
@@ -53,46 +53,46 @@ const uniaxial = 1
 const equibiaxial = 2
 const pure_shear = 3
 
+
+
 function read_data!(data_name::Int, data_type::Int)
-    base_path = joinpath(@__DIR__, "..", "data")
-    
     if data_name == Treloar_1944
         if data_type == uniaxial
-            file_path = joinpath(base_path, "TestData_Treloar", "treloar-uni.txt")
+            file_path = "TestData_Treloar/treloar-uni.txt"
         elseif data_type == equibiaxial
-            file_path = joinpath(base_path, "TestData_Treloar", "treloar-equibi.txt")
+            file_path = "TestData_Treloar/treloar-equibi.txt"
         elseif data_type == pure_shear
-            file_path = joinpath(base_path, "TestData_Treloar", "treloar-shear.txt")
+            file_path = "TestData_Treloar/treloar-shear.txt"
         else
             error("data type is not valid")
         end
     elseif data_name == Yohsuke
         if data_type == uniaxial
-            file_path = joinpath(base_path, "TestData_Yohsuke", "Yohsuke_uni.txt")
+            file_path = "TestData_Yohsuke/Yohsuke_uni.txt"
         elseif data_type == equibiaxial
-            file_path = joinpath(base_path, "TestData_Yohsuke", "Yohsuke_equibiax.txt")
+            file_path = "TestData_Yohsuke/Yohsuke_equibiax.txt"
         elseif data_type == pure_shear
-            file_path = joinpath(base_path, "TestData_Yohsuke", "Yohsuke_pure_shear.txt")
+            file_path = "TestData_Yohsuke/Yohsuke_pure_shear.txt"
         else
             error("data type is not valid")
         end
     elseif data_name == Meunier
         if data_type == uniaxial
-            file_path = joinpath(base_path, "data_Meunier", "Meunier_uniax.txt")
+            file_path = "data_Meunier/Meunier_uniax.txt"
         elseif data_type == equibiaxial
-            file_path = joinpath(base_path, "data_Meunier", "Meunier_equbiax.txt")
+            file_path = "data_Meunier/Meunier_equbiax.txt"
         elseif data_type == pure_shear
-            file_path = joinpath(base_path, "data_Meunier", "Meunier_shear.txt")
+            file_path = "data_Meunier/Meunier_shear.txt"
         else
             error("data type is not valid")
         end
     elseif data_name == Mai
         if data_type == uniaxial
-            file_path = joinpath(base_path, "data_Mai", "Mai_unia.txt")
+            file_path = "data_Mai/Mai_unia.txt"
         elseif data_type == equibiaxial
-            file_path = joinpath(base_path, "data_Mai", "Mai_biax.txt")
+            file_path = "data_Mai/Mai_biax.txt"
         elseif data_type == pure_shear
-            file_path = joinpath(base_path, "data_Mai", "Mai_shear.txt")
+            file_path = "data_Mai/Mai_shear.txt"
         else
             error("data type is not valid")
         end
@@ -107,3 +107,5 @@ function read_data!(data_name::Int, data_type::Int)
     return λ, P
 end
 
+
+λ, P = read_data!(Meunier, equibiaxial)
